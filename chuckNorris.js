72584e28808b0bf2eee8d4cff8ffeb4a5f002621
LeonardoRomulo <leo.romulo15@gmail.com>
2025-02-11 18:46:37 -0300

    att 2.0 troca da declaração de variáveis

--- chuckNorris.js
+++ chuckNorris.js
@@ -1,5 +1,5 @@
 // coleção de objetos com as curiosidades de Chuck Norris com 10 objetos do 0 ao 9
-var curiosidades_chuck_norris=[
+let curiosidades_chuck_norris=[
     {titulo:"Cuidados com a higiene bucal",     conteudo:"Chuck Norris usa arame farpado com fio dental."},
     {titulo:"Fórmula para maratonar séries",    conteudo:"Chuck Norris pode assistir um episódio de 60 minutos em 22 segundos"},
     {titulo:"Suicida que não morre",            conteudo:"Chuck Norris já foi homem bomba 34 vezes."},
@@ -12,13 +12,13 @@ var curiosidades_chuck_norris=[
     {titulo:"Contando sem parar",               conteudo:"Chuck Norris contou até o infinito. Duas vezes"}
 ];
 
-var tamanho_colecao = curiosidades_chuck_norris.length // Criamos aqui uma variável para guardar o tamanho da coleção de objetos
+let tamanho_colecao = curiosidades_chuck_norris.length // Criamos aqui uma variável para guardar o tamanho da coleção de objetos
 
-var numero_sorteado = Math.floor(Math.random() * tamanho_colecao); //Criamos uma variável para guardar o número aleatório sorteado.
+let numero_sorteado = Math.floor(Math.random() * tamanho_colecao); //Criamos uma variável para guardar o número aleatório sorteado.
 
-var curiosidade_escolhida = curiosidades_chuck_norris[numero_sorteado]; // a variável curiosidade escolhida irá receber a curiosidade sorteada por meio do índice do objeto sorteado
+let curiosidade_escolhida = curiosidades_chuck_norris[numero_sorteado]; // a variável curiosidade escolhida irá receber a curiosidade sorteada por meio do índice do objeto sorteado
 
-var titulo_curiosidade = curiosidade_escolhida.titulo;// Criamos a variável para guardar o titulo da curiosidade do número aleatório que foi sorteado
+let titulo_curiosidade = curiosidade_escolhida.titulo;// Criamos a variável para guardar o titulo da curiosidade do número aleatório que foi sorteado
 var conteudo_curiosidade = curiosidade_escolhida.conteudo;//criamos a variável conteudo curiosidade para guardar o conteudo da curiosidade sorteada
 
 //Aqui imprimimos na tela a curiosidade sorteada aleatoriamente para o usuário
@@ -27,6 +27,6 @@ console.log("Título: "+titulo_curiosidade);
 console.log("Conteúdo: "+conteudo_curiosidade)
 
 
-// math.random = função nativa do js == retorna um número aleatório ente 0<1 : 1 não incluso números possíveis = 0.98786455 // 0.984576 (Math.random()* tamanho.colecao) ==> 0.984576 *10 = 9,8786455
+//math.random = função nativa do js == retorna um número aleatório ente 0<1 : 1 não incluso números possíveis = 0.98786455 // 0.984576 (Math.random()* tamanho.colecao) ==> 0.984576 *10 = 9,8786455
 //math.random = arredonda para um número abaixo para o número inteiro mais próximo ===> 9,8786455 = arredonda para 9
 
